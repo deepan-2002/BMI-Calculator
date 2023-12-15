a771bf40ca7fa2a016a9201fc25e9f8ec2f54bb2
Deeban Yathiraja R <deebanyathiraja2002@gmail.com>
2023-12-15 15:18:23 +0530

    Built UI

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,46 +3,61 @@ let weightEl = document.querySelector('.weight')
 let resultBtn = document.querySelector('button')
 let bmiResult = document.querySelector('.bmi-result')
 let healthyWeight = document.querySelector('.healthy-weight')
+let healthyHeight = document.querySelector('.healthy-height')
 let statusTxt = document.querySelector('.status')
+let result = document.querySelector('.result')
+
+result.style.display = 'none'
 
 let bmiCalci = () => {
     let height = parseInt(heightEl.value)
     let weight = parseInt(weightEl.value)
-    
+
     if (isNaN(height) || isNaN(weight)) {
-        bmiResult.textContent = "Please enter valid height and weight.";
-    } else {
+        alert('Please enter valid height and weight')
+        return false
+    }
+    else {
+        result.style.display = 'block'
+
         let bmi = weight / Math.pow(height / 100, 2);
         bmiResult.innerHTML = `Your BMI is: ${bmi.toFixed(1)} kg/m<sup>2</sup>`;
-        healthyWeight.innerHTML = `Healthy Weight for the Height is ${(18.5 * Math.pow(height / 100, 2)).toFixed(1)} kg - ${(25 * Math.pow(height / 100, 2)).toFixed(1)} kg`
+        healthyWeight.innerHTML = `Healthy Weight for the Height is <strong>${(18.5 * Math.pow(height / 100, 2)).toFixed(1)} kg - ${(25 * Math.pow(height / 100, 2)).toFixed(1)} kg </strong>`
+        healthyHeight.innerHTML = `Your Height should between <strong>${100 * Math.sqrt(weight / 25).toFixed(1)} cm - ${100 * Math.sqrt(weight / 18.5).toFixed(1)} cm </strong> for this weight`
 
-        if(bmi<= 16){
-            statusTxt.innerHTML = `Underweight`
+        if (bmi <= 16) {
+            statusTxt.innerHTML = `<p class='text-pink-800'>Underweight</p>`
         }
-        else if(bmi> 16 && bmi <= 17){
-            statusTxt.innerHTML = `Underweight`
+        else if (bmi > 16 && bmi <= 17) {
+            statusTxt.innerHTML = `<p class='text-pink-600'>Underweight</p>`
         }
-        else if(bmi> 17 && bmi < 18.5){
-            statusTxt.innerHTML = `Underweight`
+        else if (bmi > 17 && bmi < 18.5) {
+            statusTxt.innerHTML = `<p class='text-yellow-400'>Underweight</p>`
         }
-        else if(bmi>= 18.5 && bmi <= 25){
-            statusTxt.innerHTML = `Normal`
+        else if (bmi >= 18.5 && bmi <= 25) {
+            statusTxt.innerHTML = `<p class='text-green-600'>Normal</p>`
         }
-        else if(bmi> 25 && bmi <= 30){
-            statusTxt.innerHTML = `Overweight`
+        else if (bmi > 25 && bmi <= 30) {
+            statusTxt.innerHTML = `<p class='text-yellow-400'>Overweight</p>`
         }
-        else if(bmi> 30 && bmi <= 35){
-            statusTxt.innerHTML = `Obesity`
+        else if (bmi > 30 && bmi <= 35) {
+            statusTxt.innerHTML = `<p class='text-pink-600'>Obesity</p>`
         }
-        else if(bmi> 35 && bmi <= 40){
-            statusTxt.innerHTML = `Obesity`
+        else if (bmi > 35 && bmi <= 40) {
+            statusTxt.innerHTML = `<p class='text-pink-800'>Obesity</p>`
         }
-        else{
-            statusTxt.innerHTML = `Obesity`
+        else {
+            statusTxt.innerHTML = `<p class='text-pink-900'>Obesity</p>`
         }
     }
 }
 
 resultBtn.addEventListener('click', () => {
     bmiCalci()
+})
+
+weightEl.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        bmiCalci()
+    }
 })
